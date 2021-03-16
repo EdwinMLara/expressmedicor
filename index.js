02ebdb51b814c4a37e95204a1be434af4179a057
EdwinMLara <emlara35@gmail.com>
2021-03-16 14:12:29 -0600

    Agregando imagen de base de datos

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ mongoose.connect(
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000,()=>{
-    console.log(`El servidor se inicio en ${PORT}`)
+    console.log(`El servidor se inicio en ${PORT}`);
 });
