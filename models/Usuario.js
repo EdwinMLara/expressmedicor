96942d0f855b0ee4d6351713643714ddabab03b5
EdwinMLara <Valiant.ecatepec1>
2021-09-06 13:59:45 -0500

    modificando usuarios y tipos de cuentas

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
+/**
+ * Se van a genera 3 tipos de cuentas identificadas por valores númerios
+ * 1 ----- Adminstrador
+ * 2 ----- Medico Require el valor de la cedula
+ * 3 ----- Administrador de inventarios
+ */
+
 const UsuariosSchema = mongoose.Schema({
     username:{
         type:String,
@@ -8,6 +15,17 @@ const UsuariosSchema = mongoose.Schema({
     password:{
         type:String,
         require:true
+    },
+    name:{
+        type:String,
+        require:true
+    },
+    typeCount:{
+        type:String,
+        require:true
+    },
+    cedula:{
+        type:Number,
     },
     date:{
         type:Date,
--- routes/rUsuarios.js
+++ routes/rUsuarios.js
@@ -10,7 +10,10 @@ router.post('/insert',async(req,res) =>{
     
         const usuario = new Usuarios({
             username:req.body.username,
-            password:hashecPassword
+            password:hashecPassword,
+            nombre:req.body.name,
+            typeCount:req.body.typeCount,
+            cedula:req.body.cedula 
         });
 
         console.log(usuario);
@@ -22,6 +25,16 @@ router.post('/insert',async(req,res) =>{
         console.log(error);
         res.sendStatus(500);
         return
+    }
+});
+
+router.get('/',async (req,res) =>{
+    try{
+        const usuarios = await Usuarios.find();
+        res.json(usuarios);
+    }
+    catch(error){
+        res.json({message:error});
     }
 });
 
