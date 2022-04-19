8bec860a106a00c6b7f4c3944b0efbc556d5af6b
EdwinMLara <emlara35@gmail.com>
2022-04-19 18:18:20 -0500

    nothing

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const bodyParser  = require('body-parser');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 */
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
